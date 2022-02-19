07da37808f021bd6da02934198020ea109019287
Jaynesh1609 <54092967+Jaynesh1609@users.noreply.github.com>
2022-02-19 21:28:52 +0530

    creating gcp infra with tf

---
+++ vm.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.23.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.2.1"
+    }
+  }
+}
+
+
+
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "vault-test" {
+  backend = "local"
+  config = {
+    path = "../Vault-setup/terraform.tfstate"
+  }
+}
+
+data "vault_gcp_auth_backend_role" "token-vault" {
+  backend   = data.terraform_remote_state.vault-test.outputs.backend
+  role_name = data.terraform_remote_state.vault-test.outputs.role
+}
+
+provider "google" {
+  region       = "asia-south2"
+  #access_token =  data.vault_gcp_auth_backend_role.token-vault.outputs.service_account_email
+  access_token  = data.vault_gcp_auth_backend_role.token-vault
+  project      = "idyllic-vehicle-336414"
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "n1-standard-1"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
